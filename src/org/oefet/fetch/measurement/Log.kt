5c75e18044a8235aec7a39db6043ddb297c11876
William Wood <waw31@cam.ac.uk>
2020-11-12 23:30:10 +0000

    Logging update.

--- src/org/oefet/fetch/measurement/Log.kt
+++ src/org/oefet/fetch/measurement/Log.kt
@@ -5,85 +5,135 @@ import jisa.devices.*
 import jisa.experiment.Col
 import jisa.experiment.ResultStream
 import jisa.experiment.ResultTable
+import org.oefet.fetch.gui.tabs.Connections
 import org.oefet.fetch.gui.tabs.Dashboard
+import java.util.*
+import kotlin.collections.ArrayList
 
 object Log {
 
-    const val TIME_MS         = 0
-    const val GROUND_VOLTAGE  = 1
-    const val GROUND_CURRENT  = 2
-    const val SD_VOLTAGE      = 3
-    const val SD_CURRENT      = 4
-    const val SG_VOLTAGE      = 5
-    const val SG_CURRENT      = 6
-    const val FPP1_VOLTAGE    = 7
-    const val FPP2_VOLTAGE    = 8
-    const val HEATER_VOLTAGE  = 9
-    const val HEATER_CURRENT  = 10
-    const val HEATER_POWER    = 11
-    const val THERMAL_VOLTAGE = 12
-    const val FREQUENCY       = 13
-    const val X_VOLTAGE       = 14
-    const val Y_VOLTAGE       = 15
-    const val TEMPERATURE     = 16
-    const val TC_HEATER_POWER = 17
-
-    val COLUMNS =  arrayOf(
-        Col("Running Time", "s"),
-        Col("Ground Voltage", "V"),
-        Col("Ground Current", "A"),
-        Col("Source-Drain Voltage", "V"),
-        Col("Source-Drain Current", "A"),
-        Col("Source-Gate Voltage", "V"),
-        Col("Source-Gate Current", "A"),
-        Col("FPP 1 Voltage", "V"),
-        Col("FPP 2 Voltage", "V"),
-        Col("Heater Voltage", "V"),
-        Col("Heater Current", "A"),
-        Col("Heater Power", "W") { it[HEATER_VOLTAGE] * it[HEATER_CURRENT] },
-        Col("Thermal Voltage", "V"),
-        Col("Field Frequency", "Hz"),
-        Col("X Voltage", "V"),
-        Col("Y Voltage", "V"),
-        Col("Temperature", "K"),
-        Col("TC Heater Power", "%")
-    )
-
-    private var gdSMU: SMU?         = null
-    private var sdSMU: SMU?         = null
-    private var sgSMU: SMU?         = null
-    private var lockIn: DPLockIn?   = null
-    private var preAmp: VPreAmp?    = null
-    private var dcPower: DCPower?   = null
-    private var tMeter: TMeter?     = null
+    private var gdSMU: SMU? = null
+    private var sdSMU: SMU? = null
+    private var sgSMU: SMU? = null
+    private var lockIn: DPLockIn? = null
+    private var preAmp: VPreAmp? = null
+    private var dcPower: DCPower? = null
+    private var tMeter: TMeter? = null
     private var fControl: FControl? = null
-    private var tControl: TC?       = null
-    private var fpp1: VMeter?       = null
-    private var fpp2: VMeter?       = null
-    private var tvMeter: VMeter?    = null
-    private var heater: SMU?        = null
+    private var tControl: TC? = null
+    private var fpp1: VMeter? = null
+    private var fpp2: VMeter? = null
+    private var tvMeter: VMeter? = null
+    private var heater: SMU? = null
+    private var logTasks: MutableList<() -> Double> = ArrayList()
 
-    private var log    : ResultTable? = null;
-    private val logger : RTask        = RTask(2500) { it -> log(log!!, it) }
+    private var log: ResultTable? = null;
+    private val logger: RTask = RTask(2500) { it -> log(log!!, it) }
 
     fun start(path: String) {
 
-        gdSMU    = Instruments.gdSMU
-        sdSMU    = Instruments.sdSMU
-        sgSMU    = Instruments.sgSMU
-        heater   = Instruments.htSMU
-        tvMeter  = Instruments.tvMeter
-        fpp1     = Instruments.fpp1
-        fpp2     = Instruments.fpp2
-        tControl = Instruments.tControl
-        tMeter   = Instruments.tMeter
-        lockIn   = Instruments.lockIn
-        dcPower  = Instruments.dcPower
-        preAmp   = Instruments.preAmp
-        fControl = if (lockIn != null && dcPower != null) FControl(lockIn!!, dcPower!!) else null
+        logTasks.clear()
+
+        val columns = LinkedList<Col>()
+
+        for (connection in Connections.getInstrumentsByType(Instrument::class.java)) {
+
+            if (!connection.isConnected) continue
+
+            val inst = connection.get()
+
+            when (inst) {
+
+                is MCSMU -> {
+
+                    for ((channel, smu) in inst.channels.withIndex()) {
+                        columns.add(Col("${inst.javaClass.simpleName} Channel $channel Voltage", "V"))
+                        columns.add(Col("${inst.javaClass.simpleName} Channel $channel Current", "A"))
+                        logTasks.add { smu.voltage }
+                        logTasks.add { smu.current }
+                    }
+
+                }
+
+                is SMU -> {
+                    columns.add(Col("${inst.javaClass.simpleName} Voltage", "V"))
+                    columns.add(Col("${inst.javaClass.simpleName} Current", "A"))
+                    logTasks.add { inst.voltage }
+                    logTasks.add { inst.current }
+                }
+
+                is DCPower -> {
+                    columns.add(Col("${inst.javaClass.simpleName} Voltage", "V"))
+                    columns.add(Col("${inst.javaClass.simpleName} Current", "A"))
+                    logTasks.add { inst.voltage }
+                    logTasks.add { inst.current }
+                }
+
+                is VMeter -> {
+                    columns.add(Col("${inst.javaClass.simpleName} Voltage", "V"))
+                    logTasks.add { inst.voltage }
+                }
+
+                is MSMOTC -> {
+
+                    for ((sensor, tMeter) in inst.sensors.withIndex()) {
+                        columns.add(Col("${inst.javaClass.simpleName} Sensor $sensor Temperature", "K"))
+                        logTasks.add { tMeter.temperature }
+                    }
+
+                    for ((output, tc) in inst.outputs.withIndex()) {
+                        columns.add(Col("${inst.javaClass.simpleName} Output $output Heater Power", "K"))
+                        logTasks.add { tc.heaterPower }
+                    }
+
+                }
+
+                is MSTC -> {
+
+                    for ((sensor, tMeter) in inst.sensors.withIndex()) {
+                        columns.add(Col("${inst.javaClass.simpleName} Sensor $sensor Temperature", "K"))
+                        logTasks.add { tMeter.temperature }
+                    }
+
+                    columns.add(Col("${inst.javaClass.simpleName} Heater Power", "K"))
+                    logTasks.add { inst.heaterPower }
+
+                }
+
+                is TC -> {
+                    columns.add(Col("${inst.javaClass.simpleName} Temperature", "K"))
+                    columns.add(Col("${inst.javaClass.simpleName} Heater Power", "%"))
+                    logTasks.add { inst.temperature }
+                    logTasks.add { inst.heaterPower }
+                }
+
+                is TMeter -> {
+                    columns.add(Col("${inst.javaClass.simpleName} Temperature", "K"))
+                    logTasks.add { inst.temperature }
+                }
+
+                is DPLockIn -> {
+                    columns.add(Col("${inst.javaClass.simpleName} X Voltage", "V"))
+                    columns.add(Col("${inst.javaClass.simpleName} Y Voltage", "V"))
+                    columns.add(Col("${inst.javaClass.simpleName} Frequency", "Hz"))
+                    logTasks.add { inst.lockedX }
+                    logTasks.add { inst.lockedY }
+                    logTasks.add { inst.frequency }
+                }
+
+                is LockIn -> {
+                    columns.add(Col("${inst.javaClass.simpleName} Voltage", "V"))
+                    columns.add(Col("${inst.javaClass.simpleName} Frequency", "Hz"))
+                    logTasks.add { inst.lockedAmplitude }
+                    logTasks.add { inst.frequency }
+                }
+
+            }
+
+        }
 
         logger.stop()
-        log = ResultStream(path, *COLUMNS)
+        log = ResultStream(path, *(columns.toArray(Array(0) { Col("", "") })))
         logger.start()
 
         Dashboard.watchLog(log!!)
@@ -99,25 +149,13 @@ object Log {
 
     private fun log(log: ResultTable, task: RTask) {
 
-        log.addData(
-            task.secFromStart,
-            gdSMU?.voltage        ?: 0.0,
-            gdSMU?.current        ?: 0.0,
-            sdSMU?.voltage        ?: 0.0,
-            sdSMU?.current        ?: 0.0,
-            sgSMU?.voltage        ?: 0.0,
-            sgSMU?.current        ?: 0.0,
-            fpp1?.voltage         ?: 0.0,
-            fpp2?.voltage         ?: 0.0,
-            heater?.voltage       ?: 0.0,
-            heater?.current       ?: 0.0,
-            tvMeter?.voltage      ?: 0.0,
-            lockIn?.frequency     ?: 0.0,
-            lockIn?.lockedX       ?: 0.0,
-            lockIn?.lockedY       ?: 0.0,
-            tMeter?.temperature   ?: 0.0,
-            tControl?.heaterPower ?: 0.0
-        )
+        val data = DoubleArray(logTasks.size)
+
+        for ((i, logTask) in logTasks.withIndex()) {
+            data[i] = logTask()
+        }
+
+        log.addData(*data)
 
     }
 
