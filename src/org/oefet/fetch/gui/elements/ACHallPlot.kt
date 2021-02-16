a1462c573c58e5081cb333a936de1fe7ebdf0783
William Wood <waw31@cam.ac.uk>
2021-02-16 10:21:40 +0000

    Updated JISA

--- src/org/oefet/fetch/gui/elements/ACHallPlot.kt
+++ src/org/oefet/fetch/gui/elements/ACHallPlot.kt
@@ -4,6 +4,7 @@ import jisa.experiment.ResultTable
 import jisa.gui.Series.Dash.DASHED
 import jisa.gui.Series.Dash.DOTTED
 import jisa.maths.matrices.RealMatrix
+import org.oefet.fetch.measurement.ACHall
 import org.oefet.fetch.measurement.ACHall.Companion.HALL_ERROR
 import org.oefet.fetch.measurement.ACHall.Companion.HALL_VOLTAGE
 import org.oefet.fetch.measurement.ACHall.Companion.SD_CURRENT
@@ -18,21 +19,10 @@ class ACHallPlot(data: ResultTable, optimised: RealMatrix? = null, faraday: Real
         pointOrdering  = Sort.ORDER_ADDED
 
         createSeries()
-            .setName("X Voltage")
-            .setMarkerVisible(false)
-            .setLineDash(DOTTED)
-            .watch(data, SD_CURRENT, X_VOLTAGE)
-
-        createSeries()
-            .setName("Y Voltage")
-            .setMarkerVisible(false)
-            .setLineDash(DOTTED)
-            .watch(data, SD_CURRENT, Y_VOLTAGE)
-
-        createSeries()
             .setName("Vector Subtracted")
             .polyFit(1)
             .watch(data, SD_CURRENT, HALL_VOLTAGE, HALL_ERROR)
+            .split(ACHall.FREQUENCY, "VS (f = %s Hz)")
 
         if (optimised != null) {
 
@@ -40,6 +30,7 @@ class ACHallPlot(data: ResultTable, optimised: RealMatrix? = null, faraday: Real
                 .setName("Phase Optimised")
                 .polyFit(1)
                 .addPoints(data.getColumns(SD_CURRENT), optimised, data.getColumns(HALL_ERROR))
+                .split(ACHall.FREQUENCY, "PO (f = %s Hz)")
 
         }
 
@@ -50,6 +41,7 @@ class ACHallPlot(data: ResultTable, optimised: RealMatrix? = null, faraday: Real
                 .setMarkerVisible(false)
                 .setLineDash(DASHED)
                 .addPoints(data.getColumns(SD_CURRENT), faraday)
+                .split(ACHall.FREQUENCY, "VF (f = %s Hz)")
 
         }
 
