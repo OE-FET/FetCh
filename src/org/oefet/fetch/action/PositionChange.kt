3725bb9b4b673ec38848b220a98cf1cd0a0948d4
William Wood <waw31@cam.ac.uk>
2021-07-02 15:14:53 +0100

    Updated JISA jar file.

--- src/org/oefet/fetch/action/PositionChange.kt
+++ src/org/oefet/fetch/action/PositionChange.kt
@@ -11,30 +11,14 @@ import org.oefet.fetch.gui.elements.FetChPlot
 
 class PositionChange : FetChAction("Change Position") {
 
-    var task: RTask? = null
-
-    val interval      by input("Position", "Logging Interval [s]", 0.5) map { it.toMSec().toLong() }
     val pControl      by requiredConfig("Position Controller", ProbeStation::class)
-    val xposition    by input("Position", "x Position [m]", 1e-3)
-    val yposition    by input("Position", "y Position [m]", 1e-3)
-    val zposition    by input("Position", "z Position [m]", 1e-3)
+    val xposition     by input("Position", "x Position [m]", 1e-3)
+    val yposition     by input("Position", "y Position [m]", 1e-3)
+    val zposition     by input("Position", "z Position [m]", 1e-3)
 
     override fun createPlot(data: ResultTable): FetChPlot {
 
-        val plot =  FetChPlot("Change Position to ($yposition TBD, $xposition TBD)", "Time [s]", "Position")
-
-        plot.createSeries()
-            .watch(data, { it[0] }, { xposition })
-            .setMarkerVisible(false)
-            .setLineWidth(1.0)
-            .setLineDash(Series.Dash.DASHED)
-            .setColour(Colour.GREY)
-
-        plot.createSeries()
-            .watch(data, 0, 1)
-            .setMarkerVisible(false)
-            .setColour(Colour.PURPLE)
-
+        val plot =  FetChPlot("Change Position to ($yposition m, $xposition m, $zposition m)")
 
         plot.isLegendVisible = false
 
@@ -44,34 +28,18 @@ class PositionChange : FetChAction("Change Position") {
 
     override fun run(results: ResultTable) {
 
-        task = RTask(interval) { t ->
-            results.addData(t.secFromStart, pControl.xPosition)
-        }
-
-        task?.start()
-
-        pControl.isLocked = false
+        pControl.isLocked  = false
+        pControl.setXYPosition(xposition, yposition)
         pControl.zPosition = zposition
-        pControl.xPosition = xposition
-        pControl.yPosition = yposition
 
     }
 
     override fun onFinish() {
-        task?.stop()
-    }
-
-    override fun getColumns(): Array<Col> {
-
-        return arrayOf(
-            Col("Time","s"),
-            Col("Position", "TBD")
-        )
-
+        
     }
 
     override fun getLabel(): String {
-        return "$xposition, $yposition"
+        return "$xposition, $yposition, $zposition"
     }
 
 }
