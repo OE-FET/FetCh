973ec86280b971642b04d96077f3013a528e084b
William Wood <waw31@cam.ac.uk>
2021-12-09 00:56:50 +0000

    Changes to AC Hall plotting.

--- src/org/oefet/fetch/gui/elements/ACHallPlot.kt
+++ src/org/oefet/fetch/gui/elements/ACHallPlot.kt
@@ -4,8 +4,10 @@ import jisa.gui.Colour
 import jisa.gui.Series
 import jisa.gui.Series.Dash.DASHED
 import jisa.results.ResultTable
+import jisa.results.Row
 import org.oefet.fetch.measurement.ACHall
 import org.oefet.fetch.results.ACHallResult
+import java.util.function.Predicate
 import kotlin.math.absoluteValue
 
 class ACHallPlot(data: ResultTable, optimised: ResultTable?) : FetChPlot("AC Hall", "Drain Current [A]", "Hall Voltage [V]") {
@@ -23,20 +25,21 @@ class ACHallPlot(data: ResultTable, optimised: ResultTable?) : FetChPlot("AC Hal
 
     init {
 
-        val dataF = if (FARADAY != null) {
-            data.filter { !it[FARADAY] }
+        val filter = if (FARADAY != null) {
+            Predicate<Row> { !it[FARADAY] }
         } else {
-            data
+            Predicate<Row> { true }
         }
 
         isMouseEnabled = true
         pointOrdering  = Sort.ORDER_ADDED
 
-        val zero by lazy { dataF.minByOrNull { it[SD_CURRENT].absoluteValue }?.get(HALL_VOLTAGE) ?: 0.0 }
+        val zero by lazy { data.filter(filter).minByOrNull { it[SD_CURRENT].absoluteValue }?.get(HALL_VOLTAGE) ?: 0.0 }
 
         createSeries()
             .setName("Vector Subtracted")
-            .watch(dataF, { it[SD_CURRENT] }, { it[HALL_VOLTAGE] - zero }, { it[HALL_ERROR] })
+            .watch(data, { it[SD_CURRENT] }, { it[HALL_VOLTAGE] - zero }, { it[HALL_ERROR] })
+            .filter(filter)
             .split(FREQUENCY, "VS (%s Hz)")
             .setColourSequence(*VS_COLOURS)
             .polyFit(1)
@@ -46,6 +49,7 @@ class ACHallPlot(data: ResultTable, optimised: ResultTable?) : FetChPlot("AC Hal
             createSeries()
                 .setName("Phase Optimised")
                 .watch(optimised, ACHallResult.ROT_CURRENT, ACHallResult.ROT_HALL, ACHallResult.ROT_ERROR)
+                .filter(filter)
                 .setMarkerShape(Series.Shape.SQUARE)
                 .split(ACHallResult.ROT_FREQUENCY, "PO (%s Hz)")
                 .setColourSequence(*PO_COLOURS)
@@ -56,6 +60,7 @@ class ACHallPlot(data: ResultTable, optimised: ResultTable?) : FetChPlot("AC Hal
                 .setMarkerVisible(false)
                 .setLineDash(DASHED)
                 .watch(optimised, ACHallResult.ROT_CURRENT, ACHallResult.ROT_FARADAY)
+                .filter(filter)
                 .split(ACHallResult.ROT_FREQUENCY, "FV (%s Hz)")
 
         }
