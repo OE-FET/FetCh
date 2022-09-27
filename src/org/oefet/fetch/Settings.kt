cddc90e1a3e75484ca0c82a8c5a41c9d6d4e31aa
William Wood <waw31@cam.ac.uk>
2022-09-27 13:17:36 +0100

    Number of columns config for dashboard.

--- src/org/oefet/fetch/Settings.kt
+++ src/org/oefet/fetch/Settings.kt
@@ -4,12 +4,12 @@ import jisa.control.ConfigFile
 
 object Settings : ConfigFile("FetCh") {
 
-    val connections         = subBlock("connections")
-    val measureBasic        = subBlock("measureBasic")
-    val inputs              = subBlock("inputs")
-    val dashboard           = subBlock("dashboard")
-    val hidden              = subBlock("hidden")
-    val actionDisplay       = subBlock("actionDisplay")
-    val logged              = subBlock("logged")
+    val connections   = subBlock("connections")
+    val measureBasic  = subBlock("measureBasic")
+    val inputs        = subBlock("inputs")
+    val dashboard     = subBlock("dashboard")
+    val hidden        = subBlock("hidden")
+    val actionDisplay = subBlock("actionDisplay")
+    val logged        = subBlock("logged")
 
 }
