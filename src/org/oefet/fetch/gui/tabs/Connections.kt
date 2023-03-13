3a0fe68259ae133bf7c6cd2c296a02115164bd1f
William Wood <waw31@cam.ac.uk>
2023-03-13 17:26:04 +0000

    Updated JISA.jar

--- src/org/oefet/fetch/gui/tabs/Connections.kt
+++ src/org/oefet/fetch/gui/tabs/Connections.kt
@@ -2,15 +2,39 @@ package org.oefet.fetch.gui.tabs
 
 import jisa.enums.Icon
 import jisa.gui.ConnectorGrid
+import jisa.logging.Logger
+import jssc.SerialPort
+import jssc.SerialPortList
 import org.oefet.fetch.Settings
 
 object Connections : ConnectorGrid("Connections", 3) {
 
 
     init {
+
         numColumns = 3
+
         setIcon(Icon.CONNECTION)
+
         linkToConfig(Settings.connections)
+
+        addToolbarButton("Clear All Serial Ports") {
+
+            SerialPortList.getPortNames().map(::SerialPort).forEach {
+
+                Logger.addMessage("Clearing Serial Port \"$it.portName\"...")
+
+                try {
+                    it.purgePort(SerialPort.PURGE_RXABORT or SerialPort.PURGE_TXABORT or SerialPort.PURGE_RXCLEAR or SerialPort.PURGE_TXCLEAR)
+                    it.closePort()
+                } catch (e: Throwable) {
+                    Logger.addError(e.message)
+                    e.printStackTrace()
+                }
+
+            }
+        }
+
     }
 
 }
